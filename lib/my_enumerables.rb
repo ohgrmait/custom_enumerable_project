283fd427f83aa43d3d304fa3e9297d59fd441cd7
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-11 15:53:24 +0530

    Solve exercises in the "Custom Enumerables" lesson

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,73 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    self.length.times do |n|
+      yield self[n], n
+    end
+    
+    self
+  end
+
+  def my_select
+    arr = []
+    self.my_each do |elem|
+      arr << elem if yield(elem)
+    end
+
+    arr
+  end
+
+  def my_all?
+    self.my_each do |elem|
+      return false unless yield(elem)
+    end
+
+    true
+  end
+
+  def my_any?
+    self.my_each do |elem|
+      return true if yield(elem)
+    end
+
+    false
+  end
+
+  def my_none?
+    self.my_each do |elem|
+      return false if yield(elem)
+    end
+
+    true
+  end
+
+  def my_count
+    return self.size unless block_given?
+
+    counter = 0
+    self.my_each do |elem|
+      counter += 1 if yield(elem)
+    end
+
+    counter
+  end
+
+  def my_map
+    my_self = []
+    self.my_each do |elem|
+      my_self << yield(elem)
+    end
+
+    my_self
+  end
+
+  def my_inject(acc = nil, &my_block)
+    self.my_each do |elem|
+      acc = my_block.call(acc, elem)
+    end
+
+    acc
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +76,11 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    self.length.times do |n|
+      yield(self[n])
+    end
+
+    self
+  end
 end
